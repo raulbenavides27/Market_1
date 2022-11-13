4c977b2df235f1fa4f261082f75d26601c971207
raulbenavides27 <ra.benavides@duocuc.cl>
2022-11-13 13:17:47 -0300

    actualizando validacion de registro

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Form, FormBuilder, FormGroup,FormControl,Validators } from '@angular/forms';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -9,16 +10,31 @@ import { Form, FormBuilder, FormGroup,FormControl,Validators } from '@angular/fo
 export class RegistroPage implements OnInit {
 
   formularioRegistro: FormGroup;
-  constructor(public fb: FormBuilder) {
-    
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController) {
     this.formularioRegistro = this.fb.group({
       'nombre': new FormControl("",Validators.required),
       'contraseña': new FormControl("",Validators.required),
       'confirmacion': new FormControl("",Validators.required)
-    })
+    });
    }
 
   ngOnInit() {
   }
+async guardar(){
+  var f =this.formularioRegistro.value;
 
+  if(this.formularioRegistro.invalid){
+    const alert = await this.alertController.create({
+      header:'Formulario incompleto',
+      message: 'Complete los datos',
+      buttons: ['OK'],
+    });
+
+    await alert.present();
+    return;
+  }
+}
+  
 }
