6e73737482e01f295a97d936dcda1e03b030f487
raulbenavides27 <ra.benavides@duocuc.cl>
2023-01-09 19:55:19 -0300

    act

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
@@ -6,9 +7,30 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  
+  map: any;
+
+  @ViewChild('map', {read: ElementRef, static: false}) mapRef: ElementRef;
+
+  constructor() { }
 
 
- constructor(){}
+  //-----------------------API GOOGLE MAPS----------------------------------//
+  ionViewDidEnter(){
+    this.showMap();
+  }
+
+  showMap(){
+    const location = new google.maps.LatLng(-33.36355749279399, -70.67821198064264);
+    const options = {
+      center: location,
+      zoom: 15,
+      disableDefaultUI: true
+    }
+    this.map = new google.maps.Map(this.mapRef.nativeElement, options);
+  } //-----------------------API GOOGLE MAPS----------------------------------//
+  
+
 
  getPlace(){}
  
